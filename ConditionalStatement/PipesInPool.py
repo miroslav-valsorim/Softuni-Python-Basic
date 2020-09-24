d7a0401bae793b5c93627a620e28cb604bb18551
Mirkonito <mirkonito261198@gmail.com>
2020-09-24 14:22:40 +0300

    excercises

--- ConditionalStatement/PipesInPool.py
+++ ConditionalStatement/PipesInPool.py
@@ -1,4 +1,17 @@
-#	Първият ред съдържа числото V – Обем на басейна в литри – цяло число в интервала [1…10000].
-#	Вторият ред съдържа числото P1 – дебит на първата тръба за час – цяло число в интервала [1…5000].
-#	Третият ред съдържа числото P2 – дебит на втората тръба за час– цяло число в интервала [1…5000].
-#	Четвъртият ред съдържа числото H – часовете които работникът отсъства – реално число в интервала [1.0…24.00]
+v_in_liters = int(input())
+p1_for_hour = int(input())
+p2_for_hour = int(input())
+hours_missing = float(input())
+
+p1_work = p1_for_hour * hours_missing
+p2_work = p2_for_hour * hours_missing
+both_pipes = p1_work + p2_work
+both_in_percentage = both_pipes / v_in_liters * 100
+pipe_p1_in_percentage = p1_work / both_pipes * 100
+pipe_p2_in_percentage = p2_work / both_pipes * 100
+liters_over = both_pipes - v_in_liters
+
+if both_pipes <= v_in_liters:
+    print(f"The pool is {both_in_percentage:.2f}% full. Pipe 1: {pipe_p1_in_percentage:.2f}%. Pipe 2: {pipe_p2_in_percentage:.2f}%.")
+else:
+    print(f"For {hours_missing} hours the pool overflows with {abs(liters_over):.2f} liters.")
