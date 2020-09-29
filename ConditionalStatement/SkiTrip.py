9453d26aabc8cb3ea3d041d79a63613112c8b5e8
Mirkonito <mirkonito261198@gmail.com>
2020-09-29 18:14:43 +0300

    update

--- ConditionalStatement/SkiTrip.py
+++ ConditionalStatement/SkiTrip.py
@@ -7,60 +7,60 @@ nights = days_rest - 1
 if kind_of_room == "room for one person":
     price = nights * 18.00
     if grade == "positive":
-        price *= 0.85
-        print(f"{price:.2f}")
+        last_price = price + price * 25 / 100
+        print(f"{last_price:.2f}")
     elif grade == "negative":
-        price *= 0.90
-        print(f"{price:.2f}")
+        last_price = price - price * 10 / 100
+        print(f"{last_price:.2f}")
 elif kind_of_room == "apartment":
     price = nights * 25.00
     if nights <= 10:
-        price *= 0.70
+        prom_price = price - price * 30 / 100
         if grade == "positive":
-            price *= 0.85
-            print(f"{price:.2f}")
+            last_price = prom_price + prom_price * 25 / 100
+            print(f"{last_price:.2f}")
         elif grade == "negative":
-            price *= 0.90
-            print(f"{price:.2f}")
+            last_price = prom_price - prom_price * 10 / 100
+            print(f"{last_price:.2f}")
     elif 10 <= nights <= 15:
-        price *= 0.65
+        prom_price = price - price * 45 / 100
         if grade == "positive":
-            price *= 0.75
-            print(f"{price:.2f}")
+            last_price = prom_price + prom_price * 25 / 100
+            print(f"{last_price:.2f}")
         elif grade == "negative":
-            price = 0.90
-            print(f"{price:.2f}")
+            last_price = prom_price - prom_price * 10 / 100
+            print(f"{last_price:.2f}")
     elif nights > 15:
         prom_price = price - price * 50 / 100
         if grade == "positive":
-            price *= 0.75
-            print(f"{price:.2f}")
+            last_price = prom_price + prom_price * 25 / 100
+            print(f"{last_price:.2f}")
         elif grade == "negative":
-            price *= 0.90
-            print(f"{price:.2f}")
+            last_price = prom_price - prom_price * 10 / 100
+            print(f"{last_price:.2f}")
 elif kind_of_room == "president apartment":
     price = nights * 35.00
     if nights <= 10:
-        price *= 0.90
+        prom_price = price - price * 10 / 100
         if grade == "positive":
-            price = 0.75
-            print(f"{price:.2f}")
+            last_price = prom_price + prom_price * 25 / 100
+            print(f"{last_price:.2f}")
         elif grade == "negative":
-            price *= 0.90
-            print(f"{price:.2f}")
+            last_price = prom_price - prom_price * 10 / 100
+            print(f"{last_price:.2f}")
     elif 10 <= nights <= 15:
-        price *= 0.85
+        prom_price = price - price * 15 / 100
         if grade == "positive":
-            price *= 0.75
-            print(f"{price:.2f}")
+            last_price = prom_price + prom_price * 25 / 100
+            print(f"{last_price:.2f}")
         elif grade == "negative":
-            price *= 0.90
-            print(f"{price:.2f}")
+            last_price = prom_price - prom_price * 10 / 100
+            print(f"{last_price:.2f}")
     elif nights > 15:
-        price *= 0.80
+        prom_price = price - price * 20 / 100
         if grade == "positive":
-            price *= 0.75
-            print(f"{price:.2f}")
+            last_price = prom_price + prom_price * 25 / 100
+            print(f"{last_price:.2f}")
         elif grade == "negative":
-            price *= 0.90
-            print(f"{price:.2f}")
+            last_price = prom_price - prom_price * 10 / 100
+            print(f"{last_price:.2f}")
