24876674031fbac2f1e616b0101e8d390fc6cc1e
Mirkonito <mirkonito261198@gmail.com>
2020-10-04 12:16:12 +0300

    excercise

---
+++ ForLoop/Salary.py
@@ -0,0 +1,17 @@
+tabs = int(input())
+salary = float(input())
+
+for tab in range(tabs):
+    current_tab = input()
+    if current_tab == "Facebook":
+        salary -= 150
+    elif current_tab == "Instagram":
+        salary -= 100
+    elif current_tab == "Reddit":
+        salary -= 50
+    if salary <= 0:
+        print("You have lost your salary.")
+        break
+
+if salary > 0:
+    print(int(salary))
