0732f0fb5181ab1e6f9a0f8f9432b6537e88cfd5
Mirkonito <mirkonito261198@gmail.com>
2020-10-05 11:37:16 +0300

    excercise

---
+++ ForLoop/BackToThePast.py
@@ -0,0 +1,18 @@
+money = float(input())
+year_to_live = int(input())
+
+years_old = 0
+money_left = 0
+
+for year in range(1800, year_to_live + 1):
+    if year % 2 == 0:
+        money -= 12000
+    else:
+        years_old = year - 1782
+        money -= (12000 + years_old * 50)
+
+
+if money >= 0:
+    print(f"Yes! He will live a carefree life and will have {money:.2f} dollars left.")
+elif money < 0:
+    print(f"He will need {abs(money):.2f} dollars to survive.")
