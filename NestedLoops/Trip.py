e604ebe8d2bf9d68ae6f11a64aa2d5a5533f52f2
Mirkonito <mirkonito261198@gmail.com>
2020-10-17 12:23:29 +0300

    excercise

---
+++ NestedLoops/Trip.py
@@ -0,0 +1,12 @@
+destination = input()
+
+while destination != "End":
+    need_money = int(input())
+    money = 0
+    while money < need_money:
+        saved_money = int(input())
+        money += saved_money
+    else:
+        print(f"Going to {destination}")
+
+    destination = input()
