cade6a8c019b2f9a0d270c0afc7a08a5b630bb46
Mirkonito <mirkonito261198@gmail.com>
2020-10-18 12:33:36 +0300

    excercise

---
+++ NestedLoops/SpecialNumbers.py
@@ -0,0 +1,13 @@
+number = int(input())
+for numbers in range(1111, 9999):
+    is_Magic = True
+    number_as_string = str(numbers)
+    for digit in number_as_string:
+        if int(digit) == 0:
+            is_Magic = False
+            break
+        elif number % int(digit) != 0:
+            is_Magic = False
+            break
+    if is_Magic:
+        print(f"{number_as_string}", end=" ")
