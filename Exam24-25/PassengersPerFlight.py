317fa4060c8c3e4488d65de106284fe704c4c3ee
Mirkonito <mirkonito261198@gmail.com>
2020-10-24 15:17:12 +0300

    excercise

--- Exam24-25/PassengersPerFlight.py
+++ Exam24-25/PassengersPerFlight.py
@@ -4,9 +4,11 @@ number_of_flights = int(input())
 
 maximum_passengers = -sys.maxsize
 maximum_pass_name = ""
+entries = 0
 
 for flights in range(1, number_of_flights + 1):
     flight_company = input()
+    entries += 1
 
     total_passengers_per_company = 0
     number_of_current_flights = 0
@@ -14,15 +16,25 @@ for flights in range(1, number_of_flights + 1):
 
     command = input()
     while command != "Finish":
+
         number_of_passengers = int(command)
         number_of_current_flights += 1
         total_passengers_per_company += number_of_passengers
         average = total_passengers_per_company / number_of_current_flights
-        command = input()
-
         if average > maximum_passengers:
             maximum_passengers = average
             maximum_pass_name = flight_company
 
-    print(f"{flight_company}: {floor(average)} passengers.")
-print(f"{maximum_pass_name} has most passengers per flight: {floor(maximum_passengers)}")
+    if command == "Finish":
+        print(f"{flight_company}: {floor(average)} passengers.")
+        if entries == number_of_flights:
+            break
+
+        command = input()
+
+    if entries == number_of_flights:
+        print(f"{maximum_pass_name} has most passengers per flight: {floor(maximum_passengers)}")
+        break
+
+
+
