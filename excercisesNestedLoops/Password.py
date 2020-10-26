1479e6735c0b7684424f8ba223188766da0f0cd0
Mirkonito <mirkonito261198@gmail.com>
2020-10-26 19:16:48 +0200

    excercise

--- excercisesNestedLoops/Password.py
+++ excercisesNestedLoops/Password.py
@@ -2,10 +2,8 @@ number_one = int(input())
 number_two = int(input())
 number_three = int(input())
 
-for one in range(1, number_one + 1):
-    if one % 2 == 0:
-        if 2 <= number_two <= 7:
-            for two in range(1, number_two + 1):
-                for three in range(1, number_three):
-                    if three % 2 == 0:
-                        print(f"{one}{two}{three}")
+for one in range(2, number_one + 1, 2):
+    for two in range(2, number_two + 1):
+        for three in range(2, number_three + 1, 2):
+            if two == 2 or two == 3 or two == 5 or two == 7:
+                print(f"{one} {two} {three}")
